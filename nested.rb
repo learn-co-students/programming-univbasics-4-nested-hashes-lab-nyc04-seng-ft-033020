4061957259a6a12218426a6e1bc50afbafa2144f
jiangmichael1 <jiangm1@yahoo.com>
2020-03-09 20:44:31 -0400

    Done via vscode

--- nested.rb
+++ nested.rb
@@ -36,7 +36,8 @@ def alan_kay_is_known_for
           :languages => ["C"]
         }
      }
-programmer_hash[:alan_kay][:known_for]
+
+     programmer_hash[:alan_kay][:known_for]
 end
 
 def dennis_ritchies_language
@@ -55,7 +56,9 @@ def dennis_ritchies_language
           :languages => ["C"]
         }
      }
+
 programmer_hash[:dennis_ritchie][:languages][0]
+
 end
 
 def adding_matz
@@ -81,10 +84,11 @@ def adding_matz
         }
      }
 
-programmer_hash[:yukihiro_matsumoto] = {} 
+programmer_hash[:yukihiro_matsumoto] = {}
 programmer_hash[:yukihiro_matsumoto][:known_for] = "Ruby"
-programmer_hash[:yukihiro_matsumoto][:languages] = ["LISP", "C"]
+programmer_hash[:yukihiro_matsumoto][:languages] = ["Lisp", "C"]
 programmer_hash
+
 end
 
 def changing_alan
@@ -106,8 +110,9 @@ def changing_alan
      #change what Alan Kay is :known_for to the value of the alans_new_info variable. 
      alans_new_info = "GUI"
      
-programmer_hash[:alan_kay][:known_for] = "GUI"
+programmer_hash[:alan_kay][:known_for] = alans_new_info
 programmer_hash
+
 end
 
 def adding_to_dennis
@@ -126,6 +131,8 @@ def adding_to_dennis
           :languages => ["C"]
         }
      }
-programmer_hash[:dennis_ritchie][:languages] << 'Assembly'
+
+programmer_hash[:dennis_ritchie][:languages] << "Assembly"
 programmer_hash
+
 end
